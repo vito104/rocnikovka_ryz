b952782318f3d78c46c1611bc451084e5903f0e5
Boby1256 <135050386+Boby1256@users.noreply.github.com>
2023-05-30 22:13:40 +0200

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,1 +1,20 @@
-
+INSERT INTO dok(nazev_doku, id_pristavu) VALUES 
+("Dok Petra Tupce", 2)
+INSERT INTO dok(nazev_doku, id_pristavu) VALUES
+("Haubt", 3)
+INSERT INTO dok(nazev_doku, id_pristavu) VALUES
+("P1", 9)
+INSERT INTO dok(nazev_doku, id_pristavu) VALUES
+("Marina", 8)
+INSERT INTO dok(nazev_doku, id_pristavu) VALUES
+("Doka", 10)
+INSERT INTO dok(nazev_doku, id_pristavu) VALUES
+("First dock", 12)
+INSERT INTO dok(nazev_doku, id_pristavu) VALUES
+("City dock", 4)
+INSERT INTO dok(nazev_doku, id_pristavu) VALUES
+("Přístaviště", 5)
+INSERT INTO dok(nazev_doku, id_pristavu) VALUES
+("Dok Petra Sýkory", 6)
+INSERT INTO dok(nazev_doku, id_pristavu) VALUES
+("Petrovský dok", 7)
