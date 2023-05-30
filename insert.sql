4249e1bcc73f77e0c3f85f209d03e27c5d9f5bcb
Vít Ružička <74555761+vito104@users.noreply.github.com>
2023-05-30 22:14:22 +0200

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,20 +1,11 @@
 INSERT INTO dok(nazev_doku, id_pristavu) VALUES 
-("Dok Petra Tupce", 2)
-INSERT INTO dok(nazev_doku, id_pristavu) VALUES
-("Haubt", 3)
-INSERT INTO dok(nazev_doku, id_pristavu) VALUES
-("P1", 9)
-INSERT INTO dok(nazev_doku, id_pristavu) VALUES
-("Marina", 8)
-INSERT INTO dok(nazev_doku, id_pristavu) VALUES
-("Doka", 10)
-INSERT INTO dok(nazev_doku, id_pristavu) VALUES
-("First dock", 12)
-INSERT INTO dok(nazev_doku, id_pristavu) VALUES
-("City dock", 4)
-INSERT INTO dok(nazev_doku, id_pristavu) VALUES
-("Přístaviště", 5)
-INSERT INTO dok(nazev_doku, id_pristavu) VALUES
-("Dok Petra Sýkory", 6)
-INSERT INTO dok(nazev_doku, id_pristavu) VALUES
-("Petrovský dok", 7)
+("Dok Petra Tupce", 2),
+("Haubt", 3),
+("P1", 9),
+("Marina", 8),
+("Doka", 10),
+("First dock", 12),
+("City dock", 4),
+("Přístaviště", 5),
+("Dok Petra Sýkory", 6),
+("Petrovský dok", 7);
