80daff0c22bfbcf7793988858c2ebbc5eaf57c61
Vít Ružička <74555761+vito104@users.noreply.github.com>
2023-05-31 23:19:18 +0200

    Update ryz.sql

--- ryz.sql
+++ ryz.sql
@@ -3,7 +3,7 @@ use ryz;
 
 CREATE TABLE vyrobce(
     id_vyrobce INT PRIMARY KEY AUTO_INCREMENT,
-    nazev VARCHAR(50) NOT NULL UNIQUE
+    nazev_kvality VARCHAR(50) NOT NULL UNIQUE
 );
 INSERT INTO vyrobce(nazev) VALUES
 ("Shell"),
